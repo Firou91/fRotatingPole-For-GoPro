a35098ddc9edb8ee6db08dabaaaa564f07879d11
Firou91 <elies.fali.pro@gmail.com>
2023-05-31 22:13:20 +0200

    update: better orga

--- rotation/config.h
+++ rotation/config.h
@@ -8,17 +8,37 @@ const int PIN_SENS_MOTOR_X = 13; // Constante pour la direction / sens broche D1
 const int PIN_VITESSE_MOTOR_Y = 3; // Constante pour la broche vitesse moteur 2 ()
 const int PIN_SENS_MOTOR_Y = 12; // Constante pour la broche sens moteur 2 ()
 
-const int PIN_BOUTON_SENS_1_MOTOR_X = 7; // Broche pour le bouton de sens 1 du Grove Dual Button
-const int PIN_BOUTON_SENS_2_MOTOR_X = 8; // Broche pour le bouton de sens 2 du Grove Dual Button
+const int PIN_BOUTON_SENS_1_MOTOR_X = 8; // Broche pour le bouton de sens 1 du Grove Dual Button
+const int PIN_BOUTON_SENS_2_MOTOR_X = 7; // Broche pour le bouton de sens 2 du Grove Dual Button
 const int PIN_BOUTON_SENS_1_MOTOR_Y = 5; // Broche pour le bouton de sens 1 du Grove Dual Button
 const int PIN_BOUTON_SENS_2_MOTOR_Y = 6; // Broche pour le bouton de sens 2 du Grove Dual Button
 
 
+// EN TEST
+
 // Vitesse maximale de rotation (en rotations par minute)
 const float maxSpeedX = 1.0; // Vitesse maximale de rotation axe X (You can change this value)
 const float maxSpeedY = 1.0; // Vitesse maximale de rotation axe Y (You can change this value)
-
 // Variables de vitesse des moteurs
 int speedMotors = 300; // Vitesse des moteurs (c.f fiche technique moteur)
 int RPM_MOTOR_X = (speedMotors / 255) * maxSpeedX; // Constante pour la vitesse moteur 1 (DO NOT TOUCH)
 int RPM_MOTOR_Y = (speedMotors / 255) * maxSpeedY; // Constante pour la vitesse moteur 2 (DO NOT TOUCH)
+
+// Angles de rotation
+const float DEFAULT_ANGLE_X = 0.0; // Angle par défaut pour le moteur X
+const float MIN_ANGLE_X = -90.0; // Angle minimal pour le moteur X
+const float MAX_ANGLE_X = 90.0; // Angle maximal pour le moteur X
+// 
+const float DEFAULT_ANGLE_Y = 0.0; // Angle par défaut pour le moteur Y
+const float MIN_ANGLE_Y = -90.0; // Angle minimal pour le moteur Y
+const float MAX_ANGLE_Y = 90.0; // Angle maximal pour le moteur Y
+// 
+float currentAngleX = DEFAULT_ANGLE_X; // Angle actuel du moteur X
+float currentAngleY = DEFAULT_ANGLE_Y; // Angle actuel du moteur Y
+
+// Variables pour le double-clic
+const int DOUBLE_CLIC_DELAY = 500; // Délai maximal (en millisecondes) pour effectuer un double-clic
+// 
+unsigned long lastButtonClickTime = 0; // Temps du dernier clic
+bool isFirstButtonClick = true; // Indique si le premier clic d'un double-clic a été effectué
+int rotaPriority = 1; // Variable pour contrôler le moteur actif (1: moteur 1, 2: moteur 2)
--- rotation/rotation.ino
+++ rotation/rotation.ino
@@ -69,102 +69,155 @@ void controlMotorX(int sensRotaMotorX) {
   switch (sensRotaMotorX) {
     case 1:
       Serial.println("Moteur X sens 1 ");
-      digitalWrite(PIN_SENS_MOTOR_X, LOW); // Sens 1
+      digitalWrite(PIN_SENS_MOTOR_X, HIGH); // Sens 1
       analogWrite(PIN_VITESSE_MOTOR_X, 255); // Vitesse maximale à faire varier entre 0 et 255
-      Serial.println("Le moteur X tourne dans le sens 1")
+      // Serial.println("Le moteur X tourne dans le sens 1");
       break;
     case 2:
       Serial.println("Moteur X sens 2 ");
-      digitalWrite(PIN_SENS_MOTOR_X, HIGH); // Sens 2
+      digitalWrite(PIN_SENS_MOTOR_X, LOW); // Sens 2
       analogWrite(PIN_VITESSE_MOTOR_X, 255); // Vitesse maximale
-      Serial.println("Le moteur X tourne dans le sens 2")
+      // Serial.println("Le moteur X tourne dans le sens 2");
       break;
     default:
-      digitalWrite(PIN_SENS_MOTOR_X, LOW); // Aucun sens sélectionné, arrêt du moteur
       analogWrite(PIN_VITESSE_MOTOR_X, 0); // Vitesse nulle
-      Serial.println("Le moteur X est à l'arrêt")
+      //Serial.println("Le moteur X est à l'arrêt");
       break;
   }
 }
-
 void controlMotorY(int sensRotaMotorY) {
-  
   switch (sensRotaMotorY) {
     case 1:
-      Serial.println("Moteur B sens 1 ");
+      Serial.println("Moteur Y sens 1 ");
       digitalWrite(PIN_SENS_MOTOR_Y, LOW); // Sens 1
       analogWrite(PIN_VITESSE_MOTOR_Y, 255); // Vitesse maximale à faire varier entre 0 et 255
-      Serial.println("Le moteur Y tourne dans le sens 1")
+      // Serial.println("Le moteur Y tourne dans le sens 1");
       break;
     case 2:
-      Serial.println("Moteur B sens 2 ");
+      Serial.println("Moteur Y sens 2 ");
       digitalWrite(PIN_SENS_MOTOR_Y, HIGH); // Sens 2
       analogWrite(PIN_VITESSE_MOTOR_Y, 255); // Vitesse maximale
-      Serial.println("Le moteur Y tourne dans le sens 2")
+      // Serial.println("Le moteur Y tourne dans le sens 2");
       break;
     default:
-      digitalWrite(PIN_SENS_MOTOR_Y, LOW); // Aucun sens sélectionné, arrêt du moteur
       analogWrite(PIN_VITESSE_MOTOR_Y, 0); // Vitesse nulle
-      Serial.println("Le moteur Y est à l'arrêt")
+      //Serial.println("Le moteur Y est à l'arrêt");
       break;
   }
 }
 
 
+// EN TEST
+
+// █▀ █▀█ █▀▀ █▀▀ █▀▄   █░░ █ █▀▄▀█ █ ▀█▀
+// ▄█ █▀▀ ██▄ ██▄ █▄▀   █▄▄ █ █░▀░█ █ ░█░
+
+// void moveMotorX(float speedRPM) {
+//   // Vérifie si la vitesse donnée dépasse la vitesse maximale autorisée
+//   if (speedRPM > MAX_SPEED_X) {
+//     speedRPM = MAX_SPEED_X;
+//   }
+
+//   // Convertit la vitesse en tours par minute en valeur de vitesse appropriée pour la fonction analogWrite()
+//   int speedValue = map(speedRPM, 0, MAX_SPEED_X, 0, 255);
+
+//   // Détermine le sens de rotation en fonction de la valeur de vitesse
+//   if (speedValue > 0) {
+//     digitalWrite(PIN_SENS_MOTOR_X, HIGH); // Sens 1
+//   } else {
+//     digitalWrite(PIN_SENS_MOTOR_X, LOW); // Sens 2
+//   }
+
+//   // Met à jour la vitesse du moteur
+//   analogWrite(PIN_VITESSE_MOTOR_X, abs(speedValue));
+// }
+
+// void moveMotorY(float speedRPM) {
+//   // Vérifie si la vitesse donnée dépasse la vitesse maximale autorisée
+//   if (speedRPM > MAX_SPEED_Y) {
+//     speedRPM = MAX_SPEED_Y;
+//   }
+
+//   // Convertit la vitesse en tours par minute en valeur de vitesse appropriée pour la fonction analogWrite()
+//   int speedValue = map(speedRPM, 0, MAX_SPEED_Y, 0, 255);
+
+//   // Détermine le sens de rotation en fonction de la valeur de vitesse
+//   if (speedValue > 0) {
+//     digitalWrite(PIN_SENS_MOTOR_Y, HIGH); // Sens 1
+//   } else {
+//     digitalWrite(PIN_SENS_MOTOR_Y, LOW); // Sens 2
+//   }
+
+//   // Met à jour la vitesse du moteur
+//   analogWrite(PIN_VITESSE_MOTOR_Y, abs(speedValue));
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const int PIN_VITESSE_MOTEUR1 = 3; // Constante pour la broche vitesse moteur 1 (D3)
-// const int PIN_SENS_MOTEUR1 = 12; // Constante pour la broche sens moteur 1 (D12)
-// const int PIN_VITESSE_MOTEUR2 = 11; // Constante pour la broche vitesse moteur 2 ()
-// const int PIN_SENS_MOTEUR2 = 13; // Constante pour la broche sens moteur 2 ()
-// const int PIN_BOUTON_SENS_1 = 7; // Broche pour le bouton de sens 1 du Grove Dual Button
-// const int PIN_BOUTON_SENS_2 = 8; // Broche pour le bouton de sens 2 du Grove Dual Button
-
-// const int DOUBLE_CLIC_DELAY = 500; // Délai maximal (en millisecondes) pour effectuer un double-clic
-
-// unsigned long lastButtonClickTime = 0; // Temps du dernier clic
-// bool isFirstButtonClick = true; // Indique si le premier clic d'un double-clic a été effectué
-// int rotaPriority = 1; // Variable pour contrôler le moteur actif (1: moteur 1, 2: moteur 2)
+// █▀▄▀█ █▀█ ▀█▀ █▀█ █▀█   ▄▀█ █▄░█ █▀▀ █░░ █▀▀ █▀
+// █░▀░█ █▄█ ░█░ █▄█ █▀▄   █▀█ █░▀█ █▄█ █▄▄ ██▄ ▄█
 
 // void setup() {
-//   pinMode(PIN_VITESSE_MOTEUR1, OUTPUT);
-//   pinMode(PIN_SENS_MOTEUR1, OUTPUT);
-//   digitalWrite(PIN_SENS_MOTEUR1, LOW); // Arrêt du moteur 1
-//   analogWrite(PIN_VITESSE_MOTEUR1, 0);
-
-//   pinMode(PIN_VITESSE_MOTEUR2, OUTPUT);
-//   pinMode(PIN_SENS_MOTEUR2, OUTPUT);
-//   digitalWrite(PIN_SENS_MOTEUR2, LOW); // Arrêt du moteur 2
-//   analogWrite(PIN_VITESSE_MOTEUR2, 0);
-
-//   pinMode(PIN_BOUTON_SENS_1, INPUT_PULLUP);
-//   pinMode(PIN_BOUTON_SENS_2, INPUT_PULLUP);
-
-//   Serial.begin(9600);
+//   // Positionne les moteurs à l'angle par défaut
+//   angleMotorX(DEFAULT_ANGLE_X);
+//   angleMotorY(DEFAULT_ANGLE_Y);
 // }
+
+// void angleMotorX(float angle) {
+//   // Vérifie si l'angle donné est en dehors de la plage autorisée
+//   if (angle < MIN_ANGLE_X || angle > MAX_ANGLE_X) {
+//     // Gère l'erreur d'angle invalide
+//     Serial.println("Angle invalide pour le moteur X");
+//     return;
+//   }
+
+//   // Calcule la valeur de vitesse en fonction de l'angle (à adapter en fonction de votre configuration)
+//   int speed = map(angle, MIN_ANGLE_X, MAX_ANGLE_X, 0, 255);
+
+//   // Détermine le sens de rotation en fonction de l'angle (à adapter en fonction de votre configuration)
+//   if (angle < currentAngleX) {
+//     digitalWrite(PIN_SENS_MOTOR_X, HIGH); // Sens 1
+//   } else {
+//     digitalWrite(PIN_SENS_MOTOR_X, LOW); // Sens 2
+//   }
+
+//   // Met à jour la vitesse du moteur
+//   analogWrite(PIN_VITESSE_MOTOR_X, speed);
+
+//   // Met à jour l'angle actuel
+//   currentAngleX = angle;
+// }
+// void angleMotorY(float angle) {
+//   // Vérifie si l'angle donné est en dehors de la plage autorisée
+//   if (angle < MIN_ANGLE_Y || angle > MAX_ANGLE_Y) {
+//     // Gère l'erreur d'angle invalide
+//     Serial.println("Angle invalide pour le moteur Y");
+//     return;
+//   }
+
+//   // Calcule la valeur de vitesse en fonction de l'angle (à adapter en fonction de votre configuration)
+//   int speed = map(angle, MIN_ANGLE_Y, MAX_ANGLE_Y, 0, 255);
+
+//   // Détermine le sens de rotation en fonction de l'angle (à adapter en fonction de votre configuration)
+//   if (angle < currentAngleY) {
+//     digitalWrite(PIN_SENS_MOTOR_Y, LOW); // Sens 1
+//   } else {
+//     digitalWrite(PIN_SENS_MOTOR_Y, HIGH); // Sens 2
+//   }
+
+//   // Met à jour la vitesse du moteur
+//   analogWrite(PIN_VITESSE_MOTOR_Y, speed);
+
+//   // Met à jour l'angle actuel
+//   currentAngleY = angle;
+// }
+
+
+// █▀▄ █▀█ █░█ █▄▄ █░░ █▀▀ ▄▄ █▀▀ █░░ █ █▀▀ █▄▀
+// █▄▀ █▄█ █▄█ █▄█ █▄▄ ██▄ ░░ █▄▄ █▄▄ █ █▄▄ █░█
 
 // void loop() {
 //   int sensRotation = rotaDirection();
 //   controlMotor(sensRotation);
-//   // Autres actions à effectuer dans la boucle principale
 // }
 
 // int rotaDirection() {
